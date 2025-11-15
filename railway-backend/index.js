944804c78549ef1d1b0e4fafb0f88550e15c11c7
kishan2613 <160527834+kishan2613@users.noreply.github.com>
2025-11-16 00:22:06 +0530

    IP

--- railway-backend/index.js
+++ railway-backend/index.js
@@ -17,6 +17,6 @@ app.use('/api/email', emailRouter);
 
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0',() => {
 console.log(`Server listening on port ${PORT}`);
 });
